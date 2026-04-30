ed26c19336127619253d68dc8ef4dbf67af1ff8f
Devscript93 <milan.laubinger@gmail.com>
2026-04-30 17:25:22 +0200

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,16 +1,25 @@
 section .multiboot
-    dd 0x1BADB002
-    dd 0x0
-    dd -(0x1BADB002)
+align 4
+dd 0x1BADB002           
+dd 0x00                
+dd -(0x1BADB002 + 0x00)
 
 section .text
 global _start
 extern kernel_main
 
 _start:
+    
+    mov esp, stack_top
+    
     call kernel_main
     cli
 .hang:
     hlt
     jmp .hang
 
+section .bss
+align 16
+stack_bottom:
+    resb 16384  
+stack_top:
